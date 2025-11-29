7e809a74cfe05dbc48c878927c5238f14b5bd404
Md. Tajul Islam <143927912+TajulIslam07@users.noreply.github.com>
2025-11-29 17:24:46 +0600

    Update page.js adding something

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,6 @@ import DashboardContent from "./component/Dashboard/DashboardContent";
 
 export default function Home() {
   return (
-    <><DashboardContent /> fdsfsdfgdssd <>Docker TEST</></>
+    <><DashboardContent /> fdsfsdfgdssd <>Docker TEST!!!!!!!!!!!</></>
   );
 }
